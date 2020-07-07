b802c66ca2015cd5f0072fc5fe575e546b8d3186
eduardomaniak <67488953+eduardomaniak@users.noreply.github.com>
2020-07-07 10:14:09 -0500

    Update animations.js

--- js/animations.js
+++ js/animations.js
@@ -1,6 +1,6 @@
 var tl = new TimelineMax();
 
-tl.from(".hero-h1", .6, { ease: Power2.easeInOut, y: 100, opacity: 0}, .5);
-tl.from(".hero-h2", .6, { ease: Power2.easeInOut, y: 100, opacity: 0}, .55);
-tl.from(".hero-h4", .3, { ease: Power2.easeInOut, opacity: 0}, .95);
-tl.from(".feed-container", .7, { ease: Power2.easeOut, y: 100, opacity: 0}, .95);
+tl.from(".hero-h1", .6, { ease: Power2.easeInOut, y: 100, opacity: 0}, .55);
+tl.from(".hero-h2", .6, { ease: Power2.easeInOut, y: 100, opacity: 0}, .65);
+tl.from(".hero-h4", .3, { ease: Power2.easeInOut, opacity: 0}, 1);
+tl.from(".feed-container", .7, { ease: Power2.easeOut, y: 100, opacity: 0}, 1.1);
